68bdeacd2ae291bf37d6ec047ea87a90dfb92948
Roman Ilin <ilrose887@gmail.com>
2018-01-28 13:28:23 +0800

    get all channels func added

---
+++ smack/smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  smack
+//
+//  Created by Ильин Роман on 28.01.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Decodable {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
---
+++ smack/smack/Services/MessageService.swift
@@ -0,0 +1,49 @@
+//
+//  MessageService.swift
+//  smack
+//
+//  Created by Ильин Роман on 28.01.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                // way 1
+                if let json = try! JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+
+                    completion(true)
+//                way 2
+//                do {
+//                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
+//                } catch let error {
+//                    debugPrint(error as Any)
+//                }
+//                print(self.channels)
+                
+                } else {
+                    completion(false)
+                    debugPrint(response.result.error as Any)
+                }
+            }
+            
+        }
+    }
+}
